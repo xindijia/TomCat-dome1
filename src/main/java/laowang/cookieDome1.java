6320bbf619e8f4d8d506a1dc8c6b623a3c840260
laowang <2689645862@qq.com>
2022-06-24 09:46:36 +0800

    second commit

--- src/main/java/laowang/cookieDome1.java
+++ src/main/java/laowang/cookieDome1.java
@@ -13,6 +13,7 @@ public class cookieDome1 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Cookie cookie = new Cookie("username", "123456");
+        resp.addCookie(cookie);
     }
 
     @Override
